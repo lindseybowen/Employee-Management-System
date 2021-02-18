c1a606f19a5b2dc050da4519cd1e7e8608f607ef
lindsey Bowen <lindseyjbowen@gmail.com>
2021-02-18 10:38:53 -0500

    cleaned up index.js code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const inquirer = require("inquirer");
 let Database = require("./async-db");
-let cTable = require("console.table");
+
 
 const db = new Database({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Coding@101",
+    password: "Raffles@2002",
     database: "cms"
   });
   
